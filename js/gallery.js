79ee31b151ac6d0408ec4269de77a6e7f06ec2d0
ZollyQuail <zollyquail@gmail.com>
2023-03-20 19:33:14 +0300

    changes

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,22 @@
+import { renderThumbs } from './mini.js';
+import { createBigPicture } from './big_picture.js';
+
 const container = document.querySelector('.pictures');
 
 const renderGallery = (pictures) => {
-  debugger;
   container.addEventListener ('click', (evt) => {
     const thumb = evt.target.closest('[data-thumb-id]');
     if(!thumb) {
       return;
     }
 
+    const picture = pictures.find (
+      (item) => item.id === +thumb.dataset.thumbId
+    );
+    createBigPicture(picture);
   });
+
+  renderThumbs (pictures, container);
 };
+
+export { renderGallery };
