c221046390a10aa6c6acb08d61d670ad5bd64612
Keks <keks@htmlacademy.ru>
2023-03-26 22:13:10 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/mini.js
@@ -0,0 +1,27 @@
+const otherUsersPictures = document.querySelector('.pictures');
+const newThumbTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const createThumb = ({ url, comments, likes, description, id }) => {
+  const thumb = newThumbTemplate.cloneNode(true);
+  thumb.querySelector('.picture__img').src = url;
+  thumb.querySelector('.picture__img').alt = description;
+  thumb.querySelector('.picture__comments').textContent = comments.length;
+  thumb.querySelector('.picture__likes').textContent = likes;
+  thumb.dataset.thumbId = id;
+
+  return thumb;
+};
+
+const renderThumbs = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const thumb = createThumb(picture);
+    fragment.append(thumb);
+  });
+
+  otherUsersPictures.append(fragment);
+};
+
+export { renderThumbs };
